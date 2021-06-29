7886c3a42f95be00eb8a1eb96ed024f861fe6bbf
DESKTOP-EKEURC0\anhei <anhei19960725@yahoo.co.jp>
2021-06-29 17:40:07 +0900

    作業手順2_1の　１５～１７　完了。（2_1 全て完成）
    main.cppで行う処理（fighter.hをインクルード、内部処理を実装、ビルドして確認済）

--- C_Practice10/02_Refactoring/main.cpp
+++ C_Practice10/02_Refactoring/main.cpp
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fighter.h"
 
 int main()
 {
+	Fighter nanashi;
+
+	nanashi.hp = 10;
+	nanashi.str = 8;
+	nanashi.mag = 10;
+	nanashi.tec = 7;
+	nanashi.spd = 5;
+	nanashi.def = 3;
+	nanashi.luc = 4;
+	nanashi.mde = 6;
 
 
 	system("pause");
