5ca8d19cd56f3e87d70dc11120cac9ff307f3a21
DESKTOP-EKEURC0\anhei <anhei19960725@yahoo.co.jp>
2021-06-29 17:51:52 +0900

    作業手順2_2の　１～６　完了
    Common.h追加、fighter.h/.cppをunit.h/.cppに変更、stdio,stdlibの代わりにcommon.hをインクルード、構造体を作り、メンバ変数を宣言した同時にコンストラクタで初期化

---
+++ C_Practice10/02_Refactoring/Common.h
@@ -0,0 +1,34 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct Status
+{
+	int hp;
+	int str;
+	int mag;
+	int tec;
+	int spd;
+	int def;
+	int luc;
+	int mde;
+
+	Status()
+	{
+		hp = 1;
+		str = 1;
+		mag = 1;
+		tec = 1;
+		spd = 1;
+		def = 1;
+		luc = 1;
+		mde = 1;
+	}
+};
+
+
+#endif // !COMMON_H
+
--- C_Practice10/02_Refactoring/main.cpp
+++ C_Practice10/02_Refactoring/main.cpp
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "Common.h"
 #include "fighter.h"
 
 int main()
